2e0cbcfd3b0442825dc83ddee1ef89e99bfbf52b
Ubuntu <ubuntu@ip-172-31-88-106.ec2.internal>
2021-12-02 22:55:43 +0000

    Added Rps.swift

---
+++ Rps.swift
@@ -0,0 +1,88 @@
+//
+//  Rps.swift
+//
+//  Created by Jenoe Baltoe
+//  Created on 2021-12-02
+//  Version 1.0
+//  Copyright (c) 2021 Jenoe Balote. All rights reserved.
+//
+//  This program runs a rock paper scissors game, wherein the user
+//  plays a game of rock paper scissors against the computer.
+//
+import Foundation
+
+// This function generates a random rock, paper or scissors choice as the
+// bot's choice
+func cpuMove() -> String {
+
+    let lowerNum = 0
+    let higherNum = 2
+    let cpuInt = Int.random(in: lowerNum...higherNum)
+
+    var cpuMoveValue = ""
+
+    switch cpuInt {
+    case 0:
+      cpuMoveValue = "rock"
+    case 1:
+      cpuMoveValue = "paper"
+    case 2:
+      cpuMoveValue = "scissors"
+    default:
+      break
+    }
+
+    return cpuMoveValue
+}
+
+// This function returns who won
+func gameWinner(usrMove: String, cpuMove: String) -> String {
+
+    let gameWinnerValue: String
+
+    if usrMove == cpuMove {
+        gameWinnerValue = "It's a tie. "
+    } else if (usrMove == "rock" && cpuMove == "scissors")
+              || (usrMove == "scissors" && cpuMove == "paper")
+              || (usrMove == "paper" && cpuMove == "rock") {
+
+        gameWinnerValue = "You win! Good job!"
+    } else {
+        gameWinnerValue = "You lose... yikes."
+    }
+
+    return gameWinnerValue
+}
+
+// Takes in user input, makes sure it is rock paper or scissors, finds the
+// random bot choice and who wins. Then display it to the user.
+// User prompt
+print("This is the Rock, Paper, Scissors program.")
+print("\nChoose your move (rock, paper, scissors): ", terminator: "")
+
+// Gathers the user input and makes sure it isn't empty
+guard let usrMove = readLine(), !usrMove.isEmpty else {
+    print("Please enter a correct input.")
+    print("\nDone.")
+    exit(001)
+}
+
+// Validates that the user entered rock, paper or scissors
+switch usrMove {
+case "rock", "paper", "scissors":
+    break
+default:
+    print("Invalid input.")
+    print("\nDone.")
+    exit(001)
+}
+
+// Gets bot choice and who won
+let cpuMoveValue = cpuMove()
+let gameWinnerValue = gameWinner(usrMove: usrMove, cpuMove: cpuMoveValue)
+
+// Display it to the user
+print("The computer chose: \(cpuMoveValue)")
+print("\(gameWinnerValue)")
+
+print("\nDone.")
